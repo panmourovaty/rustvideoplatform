d743bea6493bccac034795eefd65389369875eac
Dominik Hejl <hejl@faster.cz>
2024-07-15 22:19:11 +0200

    update db

--- database_schema.sql
+++ database_schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE public.medium (
+CREATE TABLE public.media (
 	id varchar NOT NULL,
 	"name" varchar NOT NULL,
 	description text NOT NULL,
 	upload int8 DEFAULT EXTRACT(epoch FROM now()) NOT NULL,
 	"owner" varchar NOT NULL,
-	"type" varchar NOT NULL,
 	likes int8 DEFAULT 0 NOT NULL,
 	dislikes int8 DEFAULT 0 NOT NULL,
 	"views" int8 DEFAULT 0 NOT NULL,
 	public bool DEFAULT false NOT NULL,
+	"type" varchar NOT NULL,
 	CONSTRAINT videos_pk PRIMARY KEY (id)
 );
 CREATE TABLE public."comments" (
